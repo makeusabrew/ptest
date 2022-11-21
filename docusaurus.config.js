727f8f89346ce2a4ef7ca62c3fa373fc48e5a87f
Nick Payne <nick.payne@genesis.global>
2022-11-21 16:00:11 +0000

    fix copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()}`,
       },
       prism: {
         theme: lightCodeTheme,
